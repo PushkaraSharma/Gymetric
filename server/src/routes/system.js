2ef85ae0f4c4434302d73caab118185176577b42
Pushkara Sharma <pushkarasharma11@gmail.com>
2026-01-21 18:06:14 +0530

    added github action for expiry cron job

--- server/src/routes/system.js
+++ server/src/routes/system.js
@@ -1,8 +1,6 @@
 import { performExpiryChecks } from "../controllers/systemControlller.js";
 
 export async function systemRoutes(fastify) {
-    fastify.addHook('onRequest', fastify.authenticate);
-
     // post /api/system/run-expiry-check
     fastify.post('/run-expiry-check', performExpiryChecks);
 }
