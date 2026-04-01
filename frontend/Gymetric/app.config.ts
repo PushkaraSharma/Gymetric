a4c09773244d0447f1a92715ca860bb330d63d54
Pushkara Sharma <pushkarasharma11@gmail.com>
2026-04-01 12:32:25 +0530

    misc

--- frontend/Gymetric/app.config.ts
+++ frontend/Gymetric/app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     name: "GymKarta",
     slug: "GymKarta",
     scheme: "gymkarta",
-    version: "0.0.4",
+    version: "1.0.0",
     orientation: "portrait",
     userInterfaceStyle: "automatic",
     icon: "./assets/images/app-icon.png",
@@ -21,7 +21,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
         allowBackup: false,
         edgeToEdgeEnabled: true,
         googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json",
-        versionCode: 2,
+        versionCode: 3,
     },
     ios: {
         googleServicesFile: process.env.GOOGLE_SERVICES_PLIST ?? "./GoogleService-Info.plist",
